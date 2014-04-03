ae48e1256ee81bbcc6d1a1b3889412609f3d02b6
Ohmnivore <markbeiline@gmail.com>
2014-04-02 20:52:59 -0400

    Fixed a bunch of bugs!

--- source/GameClass.hx
+++ source/GameClass.hx
@@ -10,7 +10,7 @@ class GameClass extends FlxGame
 	var gameWidth:Int = 640; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 480; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = MenuState; // The FlxState the game starts with.
-	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+	var zoom:Float = 2; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
@@ -20,6 +20,8 @@ class GameClass extends FlxGame
 	 */
 	public function new()
 	{
+		OgmoLoader.initTilemaps();
+		
 		FlxG.autoPause = false;
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
